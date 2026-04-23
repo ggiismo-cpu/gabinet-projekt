e99c4fbc9e4336beaada6953a487abed7272fa65
ggiismo-cpu <ggiismo@gmail.com>
2026-04-23 09:51:52 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 "use strict";
 
 // BUMPUJ WERSJĘ przy każdej zmianie app-shell żeby wymusić odświeżenie cache
-const CACHE_VERSION = "v1.0.1";
+const CACHE_VERSION = "v1.0.2";
 const CACHE = "gabinet-mm-" + CACHE_VERSION;
 
 // ------------------------------------------------------------
